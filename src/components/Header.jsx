a9f5c8f557af9e9a00b09ea0f2bc205099a5884e
Gufran Khan <gufran3004@gmail.com>
2025-10-02 18:46:25 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header className="nav bg-gradient-to-b from-gray-800 p-5">
+      <div className="container mx-auto relative z-100">
+        <div className="logo-container">
+          <h1 className="text-2xl font-bold text-red-600">CinesphereGPT</h1>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
